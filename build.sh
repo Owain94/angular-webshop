079a0b6c95f5cfe85dff130e495621bfdbe23b2a
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-03-14 00:30:16 +0100

    Check for linux

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 if [ "$(uname)" == "Darwin" ]; then
     gsed 's/\(.*\)><\/s\(.*\)/\1 async><\/s\2/' < dist/index.html | gsed 's/\(.*\)<link\(.*\)/\1  <link\2/' | gsed 's/\(.*\)    <link\(.*\)/\1  <link\2/' | gsed 's/\(.*\)<\/head>\(.*\)/\1\n<\/head>\2/' | gsed -e 's:</script>:</script>\n:g' | gsed 's/\(.*\)<script\(.*\)/\1  <script\2/' > dist/index2.html;
     mv -f dist/index2.html dist/index.html
-else
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     sed 's/\(.*\)><\/s\(.*\)/\1 async><\/s\2/' < dist/index.html | sed 's/\(.*\)<link\(.*\)/\1  <link\2/' | sed 's/\(.*\)    <link\(.*\)/\1  <link\2/' | sed 's/\(.*\)<\/head>\(.*\)/\1\n<\/head>\2/' | sed -e 's:</script>:</script>\n:g' | sed 's/\(.*\)<script\(.*\)/\1  <script\2/' > dist/index2.html;
     mv -f dist/index2.html dist/index.html
 fi
