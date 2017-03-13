0959bfc70425a5e7746e7660cfea0ee65b26f16a
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-03-14 00:09:48 +0100

    Don't add service workers by default

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ if [ "$(uname)" == "Darwin" ]; then
     gsed 's/\(.*\)><\/s\(.*\)/\1 async><\/s\2/' < dist/index.html | gsed 's/\(.*\)<link\(.*\)/\1  <link\2/' | gsed 's/\(.*\)    <link\(.*\)/\1  <link\2/' | gsed 's/\(.*\)<\/head>\(.*\)/\1\n<\/head>\2/' | gsed -e 's:</script>:</script>\n:g' | gsed 's/\(.*\)<script\(.*\)/\1  <script\2/' > dist/index2.html;
     mv -f dist/index2.html dist/index.html
 else
-   sed 's/\(.*\)><\/s\(.*\)/\1 async><\/s\2/' < dist/index.html | sed 's/\(.*\)<link\(.*\)/\1  <link\2/' | sed 's/\(.*\)    <link\(.*\)/\1  <link\2/' | sed 's/\(.*\)<\/head>\(.*\)/\1\n<\/head>\2/' | sed -e 's:</script>:</script>\n:g' | sed 's/\(.*\)<script\(.*\)/\1  <script\2/' > dist/index2.html;
+    sed 's/\(.*\)><\/s\(.*\)/\1 async><\/s\2/' < dist/index.html | sed 's/\(.*\)<link\(.*\)/\1  <link\2/' | sed 's/\(.*\)    <link\(.*\)/\1  <link\2/' | sed 's/\(.*\)<\/head>\(.*\)/\1\n<\/head>\2/' | sed -e 's:</script>:</script>\n:g' | sed 's/\(.*\)<script\(.*\)/\1  <script\2/' > dist/index2.html;
     mv -f dist/index2.html dist/index.html
 fi
