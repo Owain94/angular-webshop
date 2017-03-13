b121a29b46d4feab964f1047a9ac1f6e730068c1
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-03-13 22:54:12 +0100

    Better way to get the head tag (thanks to AngularClass)

--- src/app/modules/transfer-state/server-transfer-state.ts
+++ src/app/modules/transfer-state/server-transfer-state.ts
@@ -1,6 +1,11 @@
 import { Injectable, Optional, RendererFactory2, ViewEncapsulation } from '@angular/core';
 import { TransferState } from './transfer-state';
 import { PlatformState } from '@angular/platform-server';
+
+export function isTag(tagName: string, node: any): boolean {
+  return node.type === 'tag' && node.name === tagName;
+}
+
 @Injectable()
 export class ServerTransferState extends TransferState {
     constructor( private state: PlatformState, private rendererFactory: RendererFactory2) {
@@ -21,19 +26,42 @@ export class ServerTransferState extends TransferState {
         data: {}
       });
 
-      let head: any;
+      let rootNode: any;
+      let headNode: any;
 
-      if (document.children[0].name === 'html') {
-        head = document.children[0].children[0];
-      } else if (document.children[1].name === 'html') {
-        head = document.children[1].children[0];
-      } else {
-        throw new Error('Please have <head> as the first element in your document');
+      // tslint:disable-next-line:no-inferrable-types
+      for (let i: number = 0; i < document.childNodes.length; ++i) {
+        const child = document.childNodes[i];
+
+        if (isTag('html', child)) {
+          rootNode = child;
+          break;
+        }
+      }
+
+      if (!rootNode) {
+        rootNode = document;
+      }
+
+      // tslint:disable-next-line:no-inferrable-types
+      for (let i: number = 0; i < rootNode.childNodes.length; ++i) {
+       const child = rootNode.childNodes[i];
+
+        if (isTag('head', child)) {
+          headNode = child;
+          break;
+        }
       }
 
       const script = renderer.createElement('script');
-      renderer.setValue(script, `window['TRANSFER_STATE'] = ${transferStateString}`);
-      renderer.appendChild(head, script);
+      renderer.setValue(script, `
+try {
+  window['TRANSFER_STATE'] = ${ transferStateString };
+} catch (e) {
+}
+      `);
+      renderer.appendChild(headNode, script);
+      renderer.setAttribute(script, 'angular', 'universal');
     } catch (e) {
       console.error(e);
     }
