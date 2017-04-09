f8a04eb2d39f16c84e08cf856ad1f59b9c3479eb
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-04-09 07:54:23 +0200

    Cannot assign to 'pattern' because it is a constant or a read-only property

--- src/app/pipes/highlight.pipe.ts
+++ src/app/pipes/highlight.pipe.ts
@@ -3,7 +3,7 @@ import {PipeTransform, Pipe} from '@angular/core';
 @Pipe({ name: 'highlight' })
 export class HighlightPipe implements PipeTransform {
   transform(text: string, search): string {
-    const pattern = search.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+    let pattern = search.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
     pattern = pattern.split(' ').filter((t) => {
       return t.length > 0;
     }).join('|');
