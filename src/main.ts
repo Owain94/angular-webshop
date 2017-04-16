6592557d1edbac9555513bd15f870676266b7246
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-04-16 13:17:02 +0200

    These aren't used anymore

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppBrowserModule } from './app/modules/app.browser.module';
-import { environment } from './environments/environment';
 
 if (process.env.ENV === 'production') {
   enableProdMode();
