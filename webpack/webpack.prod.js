c2bb6f20b18c11efee890e50510ef14d75b92b61
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-04-27 00:50:21 +0200

    NODE_ENV

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -34,9 +34,7 @@ module.exports = {
       }
     }),
     new webpack.DefinePlugin({
-      "process.env": {
-        "ENV": JSON.stringify("production")
-      }
+      "process.env.NODE_ENV": JSON.stringify('production')
     })
   ]
 };
