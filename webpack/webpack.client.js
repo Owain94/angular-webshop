58146be7bae8b1f9dfd735a3ebda72ca68a4a697
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-04-28 04:52:26 +0200

    Strings must use doublequote. (quotes) -- codacy

--- webpack/webpack.client.js
+++ webpack/webpack.client.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
 
 const { CommonsChunkPlugin } = require("webpack").optimize;
 
