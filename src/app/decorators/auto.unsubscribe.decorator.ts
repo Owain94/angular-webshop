1c0422e70ed8d5f12f6bdfe537c92cfa2dd19882
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-05-04 02:08:49 +0200

    Logging

--- src/app/decorators/auto.unsubscribe.decorator.ts
+++ src/app/decorators/auto.unsubscribe.decorator.ts
@@ -4,15 +4,24 @@ export function AutoUnsubscribe(blackList: any = []) {
     const original = constructor.prototype.ngOnDestroy;
 
     constructor.prototype.ngOnDestroy = function () {
+      if (process.env.NODE_ENV !== 'production') {
+        console.groupCollapsed(`${constructor.name} - Unsubscribe`);
+      }
       for (const prop in this) {
         if (this.hasOwnProperty(prop)) {
           const property = this[prop];
           if (blackList.indexOf(prop) === -1) {
             if (property && (typeof property.unsubscribe === 'function')) {
               property.unsubscribe();
+              if (process.env.NODE_ENV !== 'production') {
+                console.log('%c Unsubscribed', 'color: #7E57C2; font-weight: bold', property);
+              }
             }
           }
         }
+      }
+      if (process.env.NODE_ENV !== 'production') {
+        console.groupEnd();
       }
       // tslint:disable-next-line:no-unused-expression
       original && typeof original === 'function' && original.apply(this, arguments);
