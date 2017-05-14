89982e37b503cfd2b458214c469607bc0daa99e9
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-05-14 03:06:15 +0200

    More charts

--- src/app/components/admin/stats/stats.component.ts
+++ src/app/components/admin/stats/stats.component.ts
@@ -1,6 +1,6 @@
 /// <reference path="../../../interfaces/admin/total.stats.interface.ts" />
+/// <reference path="../../../interfaces/products/products.interface.ts" />
 
-import { Observable } from 'rxjs/Observable';
 import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 
 import { Log } from '../../../decorators/log.decorator';
@@ -8,12 +8,16 @@ import { AutoUnsubscribe } from '../../../decorators/auto.unsubscribe.decorator'
 import { LogObservable } from '../../../decorators/log.observable.decorator';
 
 import { AnalyticsService } from '../../../services/analytics.service';
+import { ProductService } from './../../../services/product.service';
 
 import { AdminGuard } from '../../../guards/admin.guard';
 
 import { NgDateRangePickerOptions } from 'ng-daterangepicker';
 
+import { Observable } from 'rxjs/Rx';
 import { Subscription } from 'rxjs/Subscription';
+
+import 'rxjs/add/observable/forkJoin';
 
 import '../../../../../node_modules/chart.js/dist/Chart.bundle.js';
 
@@ -26,23 +30,54 @@ import '../../../../../node_modules/chart.js/dist/Chart.bundle.js';
 @Log()
 @AutoUnsubscribe()
 export class AdminStatsComponent implements OnInit, OnDestroy {
+  @LogObservable public totalStats: Observable<totalStats.RootObject>;
 
   public options: NgDateRangePickerOptions;
   public dateRange: string;
 
-  @LogObservable public totalStats: Observable<totalStats.RootObject>;
+  public rangeDoughnutChartOptions: any = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+  public rangeDoughnutChartLabels: string[] = ['Bekeken pagina\'s', 'Bekeken producten'];
+  public rangeDoughnutChartData: number[];
 
-  public doughnutChartLabels: string[] = ['Bekeken pagina\'s', 'Bekeken producten'];
-  public doughnutChartData: number[];
+  public rangeBarChartOptions: any = {
+    scaleShowVerticalLines: true,
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [{
+        display: true,
+        ticks: {
+          stepSize: 5,
+          beginAtZero: true
+        }
+      }]
+    }
+  };
+
+  public rangePagesChartLabels: Array<String> = [
+    'Meest bekeken pagina\'s'
+  ];
+  public rangePagesChartData: Array<any>;
+  public rangeProductsChartLabels: Array<String> = [
+    'Meest bekeken producten'
+  ];
+  public rangeProductsChartData: Array<any>;
+
 
   // Countto stubs
   public users: any;
   public pages: any;
   public products: any;
+  public rangePages: any;
+  public rangeProducts: any;
 
   private analyticSubscription: Subscription;
 
   constructor(private adminGuard: AdminGuard,
+              private productService: ProductService,
               private analyticsService: AnalyticsService) {
   }
 
@@ -89,14 +124,83 @@ export class AdminStatsComponent implements OnInit, OnDestroy {
   private getStatsInRange(from: number, to: number) {
     this.analyticsService.getStatsInRange(from, to).subscribe(
         (res: Array<rangeStats.RootObject>) => {
-          this.doughnutChartData = [
-            res.filter((item: rangeStats.RootObject) => {
-              return item.hasOwnProperty('page');
-            }).length,
-            res.filter((item: rangeStats.RootObject) => {
-              return item.hasOwnProperty('product');
-            }).length
-          ];
+          const pages = res.filter((item: rangeStats.RootObject) => {
+            return item.hasOwnProperty('page');
+          });
+
+          const products = res.filter((item: rangeStats.RootObject) => {
+            return item.hasOwnProperty('product');
+          });
+
+          this.rangeDoughnutChartData = [pages.length, products.length];
+
+          let popularProducts: Array<{'id': string, 'count': number}> = [];
+          console.log(products);
+
+          for (const product of products) {
+            if (popularProducts.find(x => x.id === product.product)) {
+              popularProducts.find(x => x.id === product.product).count += 1;
+            } else {
+              popularProducts.push(
+                {
+                  'id': product.product,
+                  'count': 1
+                }
+              );
+            }
+          }
+
+          let popularPages: Array<{'name': string, 'count': number}> = [];
+
+          for (const page of pages) {
+            if (popularPages.find(x => x.name === page.page)) {
+              popularPages.find(x => x.name === page.page).count += 1;
+            } else {
+              popularPages.push(
+                {
+                  'name': page.page,
+                  'count': 1
+                }
+              );
+            }
+          }
+
+          popularProducts = popularProducts.sort(function (a, b) {
+            return b.count - a.count;
+          }).slice(0, 5);
+
+          popularPages = popularPages.sort(function (a, b) {
+            return b.count - a.count;
+          }).slice(0, 5);
+
+          const pagesData: Array<any> = [];
+          const productObservables: Array<Observable<productsInterface.RootObject>> = [];
+
+          for (const popularPage of popularPages) {
+            pagesData.push({data: [popularPage.count], label: popularPage.name});
+          }
+
+          this.rangePagesChartData = pagesData;
+
+          for (const popularProduct of popularProducts) {
+            productObservables.push(this.productService.product(popularProduct.id, true));
+          }
+
+          Observable.forkJoin(productObservables).subscribe(
+            (combinedRes) => {
+              const productData: Array<any> = [];
+              for (let i = 0; i < combinedRes.length; i++) {
+                console.group();
+                console.log(i);
+                console.log(combinedRes[i]);
+                console.log(popularProducts[i]);
+                console.groupEnd();
+                productData.push({data: [popularProducts[i].count], label: combinedRes[i].name});
+              }
+
+              this.rangeProductsChartData = productData;
+            }
+          );
         }
       );
   }
